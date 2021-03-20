d5a273fb8d0d01303dc11771cc631f075449a5c9
Roee Baram <roeebaram@gmail.com>
2021-03-20 21:54:09 +0200

    ddddd

--- PriceQuotationOfficeGuy/PriceQuotationOfficeGuy/Program.cs
+++ PriceQuotationOfficeGuy/PriceQuotationOfficeGuy/Program.cs
@@ -16,6 +16,14 @@ namespace PriceQuotationOfficeGuy
             //CreateHostBuilder(args).Build().Run();
         }
 
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
