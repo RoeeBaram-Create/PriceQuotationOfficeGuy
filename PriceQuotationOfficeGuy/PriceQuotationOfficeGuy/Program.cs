f64170306bb7b1baa956ee0250838783cf379145
Roee Baram <roeebaram@gmail.com>
2021-03-20 22:21:43 +0200

    no message

--- PriceQuotationOfficeGuy/PriceQuotationOfficeGuy/Program.cs
+++ PriceQuotationOfficeGuy/PriceQuotationOfficeGuy/Program.cs
@@ -14,6 +14,8 @@ namespace PriceQuotationOfficeGuy
         public static void Main(string[] args)
         {
             //CreateHostBuilder(args).Build().Run();
+
+           //aaa
         }
 
 
